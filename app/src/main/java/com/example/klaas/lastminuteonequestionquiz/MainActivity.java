e2106ed0114f04166427acca275800b3e38bfa57
klaas <xinirnix@gmail.com>
2017-04-13 00:49:52 +0200

    Quick Update

--- app/src/main/java/com/example/klaas/lastminuteonequestionquiz/MainActivity.java
+++ app/src/main/java/com/example/klaas/lastminuteonequestionquiz/MainActivity.java
@@ -1,8 +1,25 @@
 package com.example.klaas.lastminuteonequestionquiz;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+/**
+ * Add your package below. Package name can be found in the project's AndroidManifest.xml file.
+ * This is the package name our example uses:
+ *
+ * package com.example.android.justjava;
+ */
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
